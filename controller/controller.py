2311a7a85408c8ba9083acc882ca23bd8dd61ca6
mribeirom <marcelormartins0@gmail.com>
2025-05-01 00:25:08 -0300

    rotas de registro e login do usuario funcionando com o banco de dados

--- controller/controller.py
+++ controller/controller.py
@@ -1,5 +1,60 @@
-from flask import render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import login_user, login_required
+from model.usuario import Usuario
+from model.database import db
 
-class Controller:
-    def home(self):
-        return render_template('teste.html')
+
+# Cria o Blueprint
+controller_bp = Blueprint('controller_bp', __name__, template_folder='../view/templates')
+
+# Rota principal
+@controller_bp.route('/')
+def home():
+    return render_template('home.html')
+
+# Função normal para usar no user_loader do login_manager no app.py
+def user_loader_funcao(id):
+    usuario = db.session.query(Usuario).filter_by(id=id).first()
+    return usuario
+
+# Rota para registrar o Usuario
+@controller_bp.route('/registrar', methods=['GET', 'POST'])
+def registrar():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        senha = request.form['senha']
+
+        novo_usuario = Usuario(nome=nome, email=email, senha=senha)
+        db.session.add(novo_usuario)
+        db.session.commit()
+
+        login_user(novo_usuario)
+
+        return redirect(url_for('controller_bp.home'))
+
+    return render_template('registrar.html')
+
+# Rota para logar o Usuario
+@controller_bp.route('/logar', methods=['GET', 'POST'])
+def logar():
+    if request.method == 'POST':
+        email = request.form['email']
+        senha = request.form['senha']
+
+        usuario = db.session.query(Usuario).filter_by(email=email, senha=senha).first()
+        if not usuario:
+            return render_template('logar.html')
+
+        login_user(usuario)
+        return redirect(url_for('controller_bp.home'))
+        
+    return render_template('logar.html')
+        
+# Possivel Rota para adotar o Animal
+@controller_bp.route('/adotar')
+@login_required
+def adotar():
+    return 'me adota ai'
+    
+
--- model/adotante.py
+++
@@ -1,6 +0,0 @@
-from model.database import db
-
-class Adotante(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
